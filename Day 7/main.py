f8ca54aadd00866c9c9df0f9284ba51c01993894
mixedd69 <edijs.rozens@gmail.com>
2024-07-01 00:08:57 +0300

    Completed Day 7

--- Day 7/main.py
+++ Day 7/main.py
@@ -1,36 +1,56 @@
-# Step 3
+# Step 5
 
 import random
+import os
+from hangman_words import word_list
 
-word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
-# Testing code
-print(f"Pssst, the solution is {chosen_word}.")
+end_of_game = False
+lives = 6
+
+from hangman_art import logo, stages
+
+print(logo)
+
 
 # Create blanks
 display = []
 for _ in range(word_length):
     display += "_"
 
-# TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-end_of_game = False
-
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+
+    os.system("clear")
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(
-            f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}"
-        )
+        # print(
+        #    f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}"
+        # )
         if letter == guess:
             display[position] = letter
 
-    print(display)
+    # Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
 
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    # Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
-        print("You win!")
+        print("You win.")
+
+    print(stages[lives])
